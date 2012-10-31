d7fa4927e9dab4ea12f0b7b7bb09762fbe89a939
Anindya Bandopadhyay <anindyabandopadhyay@gmail.com>
2012-10-31 12:16:05 +0530

    added removeAll(T) method

---
+++ src/com/andy/datastructure/SingleLinkListDataObject.java
@@ -0,0 +1,23 @@
+package com.andy.datastructure;
+
+public class SingleLinkListDataObject<T> {
+	private T data;
+	private SingleLinkListDataObject<T> nextReference;
+	
+	public T getData() {
+		return data;
+	}
+
+	public void setData(T data) {
+		this.data = data;
+	}
+
+	public SingleLinkListDataObject<T> getNextReference() {
+		return nextReference;
+	}
+
+	public void setNextReference(SingleLinkListDataObject<T> nextReference) {
+		this.nextReference = nextReference;
+	}
+
+}
