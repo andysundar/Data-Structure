a296c57d2b78ea2a8f3183ca06789f00a64e35eb
Anindya Bandopadhyay <anindyabandopadhyay@gmail.com>
2012-11-03 22:44:07 +0530

    Added basic method skeleton

---
+++ src/com/andy/datastructure/list/DoubleLinkedList.java
@@ -0,0 +1,41 @@
+package com.andy.datastructure.list;
+
+import com.andy.datastructure.DoubleLinkListDataObject;
+
+public class DoubleLinkedList<T> {
+	
+	private DoubleLinkListDataObject<T> startNode;
+	private DoubleLinkListDataObject<T> lastNode;
+	
+	public boolean add(T data) {
+		return false;
+	}
+	
+	public boolean addAt(T data,int index) {
+		return false;
+	}
+	
+	public boolean remove(T data){
+		return false;
+	}
+
+	public boolean removeAt(int index){
+		return false;
+	}
+
+	public boolean removeAll(T data){
+		return false;
+	}
+	
+	public boolean removeAll() {
+		return false;
+	}
+
+	public T get(int T) {
+		return null;
+	}
+	
+	public int size() {
+		return 0;
+	}
+}
