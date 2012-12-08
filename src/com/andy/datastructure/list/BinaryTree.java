17b4d13bdfdef7464ad21753973498e0a755c1c3
Anindya Bandopadhyay <anindyabandopadhyay@gmail.com>
2012-12-08 23:16:02 +0530

    Refactored deleteNode(T) method

--- src/com/andy/datastructure/list/BinaryTree.java
+++ src/com/andy/datastructure/list/BinaryTree.java
@@ -24,42 +24,30 @@ public class BinaryTree<T extends Comparable<T>> {
 	
 	private boolean insertChild(T data) {
 		boolean isOk = false;
-		int level = 0;
-		BinaryTreeDataObject<T> element = root;
-		BinaryTreeDataObject<T> parentNode = null;
-		while(element != null) {
-			level++;
-			if(data.compareTo(element.getData()) <= 0) {
-				parentNode = element;
-				element = element.getLeftNode();
-			} else {
-				parentNode = element;
-				element = element.getRightNode();
-			}
-		}
-		
+		BinaryTreeDataObject<T> parentNode = findNodePosition(data);
+
 		if(data.compareTo(parentNode.getData()) <= 0){
-			insertLeftChild(parentNode,data,level);
+			insertLeftChild(parentNode,data);
 			isOk = true;
 		} else {
-			insertRightChild(parentNode,data,level);
+			insertRightChild(parentNode,data);
 			isOk = true;	
 		}
 		return isOk;
 	}
 
-	private void insertLeftChild(BinaryTreeDataObject<T> parentNode,T data,int level){
+	private void insertLeftChild(BinaryTreeDataObject<T> parentNode,T data){
 		BinaryTreeDataObject<T> childNode = new BinaryTreeDataObject<T>();
 		childNode.setData(data);
-		childNode.setLevel(level);
+		childNode.setLevel((parentNode.getLevel()+ 1));
 		parentNode.setLeftNode(childNode);
 		childNode.setParentNode(parentNode);
 	}
 	
-	private void insertRightChild(BinaryTreeDataObject<T> parentNode,T data,int level){
+	private void insertRightChild(BinaryTreeDataObject<T> parentNode,T data){
 		BinaryTreeDataObject<T> childNode = new BinaryTreeDataObject<T>();
 		childNode.setData(data);
-		childNode.setLevel(level);
+		childNode.setLevel((parentNode.getLevel()+ 1));
 		parentNode.setRightNode(childNode);
 		childNode.setParentNode(parentNode);
 	}
@@ -76,10 +64,70 @@ public class BinaryTree<T extends Comparable<T>> {
 		if(data == null){
 			throw new IllegalArgumentException("Null cannot be deleted.");
 		}
-		boolean isOk = false;
-		
-		numberOfNodes--;
+		BinaryTreeDataObject<T> node = findNodePosition(data);
+		boolean isOk = ((node != null) && (data.equals(node.getData())));
+		BinaryTreeDataObject<T> childNode = null;
+		if(isOk){
+			if((node.getLeftNode() == null) && (node.getRightNode() == null)) {
+				unlinkNode(node,null);
+			} else if((node.getLeftNode() != null) && (node.getRightNode() != null)) {
+				childNode = inorderSuccessor(node);
+				childNode.setParentNode(node.getParentNode());
+				childNode.setLevel(node.getLevel());
+				childNode.setRightNode(node.getRightNode());
+				childNode.setLeftNode(node.getLeftNode());
+				unlinkNode(node, childNode);
+			} else {
+				if(node.getLeftNode() != null) {
+					childNode = node.getLeftNode();
+					node.setLeftNode(null);
+				} else {
+					childNode = node.getRightNode();
+					node.setRightNode(null);
+				}
+				childNode.setParentNode(node.getParentNode());
+				unlinkNode(node, childNode);
+			}
+			numberOfNodes--;
+		}
 		return isOk;
+	}
+	
+	private BinaryTreeDataObject<T> inorderSuccessor(BinaryTreeDataObject<T> deleteNode) {
+		
+		return null;
+	}
+	
+	private void unlinkNode(BinaryTreeDataObject<T> deleteNode,BinaryTreeDataObject<T> parentNodeChildLink){
+		deleteNode.setData(null);
+		BinaryTreeDataObject<T> parentNode = deleteNode.getParentNode();
+		if(parentNode != null){
+			if(deleteNode.equals(parentNode.getLeftNode())){
+				parentNode.setLeftNode(parentNodeChildLink);
+			} else {
+				parentNode.setRightNode(parentNodeChildLink);
+			}
+		}
+		deleteNode.setLevel(0);
+		deleteNode.setParentNode(null);
+		deleteNode.setLeftNode(null);
+		deleteNode.setRightNode(null);
+	}
+	protected BinaryTreeDataObject<T> findNodePosition(T data){
+		BinaryTreeDataObject<T> element = root;
+		BinaryTreeDataObject<T> parentNode = null;
+		if(element != null){
+			while(element != null) {
+				if(data.compareTo(element.getData()) <= 0) {
+					parentNode = element;
+					element = element.getLeftNode();
+				} else {
+					parentNode = element;
+					element = element.getRightNode();
+				}
+			}
+		}
+		return parentNode;
 	}
 	
 	public T[] visitPreOrder() {
