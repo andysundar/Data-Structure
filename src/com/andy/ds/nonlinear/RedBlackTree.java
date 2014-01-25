df0902d8131dedca527393f7cd83cb01f697308c
Anindya Bandopadhyay <anindyabandopadhyay@gmail.com>
2014-01-25 16:35:13 +0530

    Red Black tree

--- src/com/andy/ds/nonlinear/RedBlackTree.java
+++ src/com/andy/ds/nonlinear/RedBlackTree.java
@@ -8,6 +8,25 @@
 
 package com.andy.ds.nonlinear;
 
+import com.andy.adt.TreeDataObject;
+
 public class RedBlackTree<T extends Comparable<T>> extends BinaryTree<T> {
 
+	public TreeDataObject<T> rotateLeft(TreeDataObject<T> h){
+		TreeDataObject<T> temp = h.getRightNode();
+		h.setRightNode(temp.getLeftNode());
+		temp.setLeftNode(h);
+		temp.setColour(h.getColour());
+		h.setColour(TreeDataObject.RED);
+		return temp;
+	}
+	
+	public TreeDataObject<T> rotateRight(TreeDataObject<T> h){
+		TreeDataObject<T> temp = h.getLeftNode();
+		h.setLeftNode(temp.getRightNode());
+		temp.setRightNode(h);
+		temp.setColour(h.getColour());
+		h.setColour(TreeDataObject.RED);
+		return temp;
+	}
 }
