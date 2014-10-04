ee46f8dfc711ed86a71b429b06f991e27ab59a79
Anindya Bandopadhyay <anindyabandopadhyay@gmail.com>
2014-10-04 21:04:28 +0530

    Added Stack

---
+++ src/com/andy/ds/linear/Stack.java
@@ -0,0 +1,51 @@
+/**
+ * Copyright [2012] Anindya Bandopadhyay
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.andy.ds.linear;
+
+import com.andy.adt.SingleLinkedRefDataObject;
+
+public class Stack<E> {
+
+  private LinkedList<E> stack = new LinkedList<E>();
+
+  public E push(E data){
+    stack.add(data);
+    return data;
+  }
+  
+  public E pop(){
+    E data = peek();
+    stack.removeAt((stack.size() - 1));
+    return data;
+  }
+  
+  public E peek(){
+    SingleLinkedRefDataObject<E> node = stack.getLastNode();
+    if(node == null) {
+      return null;
+    }
+    return node.getData();
+  }
+  
+  public int size() {
+    return stack.size();
+  }
+
+  public boolean isEmpty() {
+    return stack.isEmpty();
+  }
+}
