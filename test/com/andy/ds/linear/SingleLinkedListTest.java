6d1e33d00f7b376dc7f5e311bf1f29e080820071
Anindya Bandopadhyay <anindyabandopadhyay@gmail.com>
2014-12-20 19:59:07 +0530

    added test for to string when list is empty

--- test/com/andy/ds/linear/SingleLinkedListTest.java
+++ test/com/andy/ds/linear/SingleLinkedListTest.java
@@ -543,4 +543,9 @@ public class SingleLinkedListTest {
     addDataToList();
     assertEquals("[0, 1, 2, 3, 4, 5, 6, 7, 8, 9]",singleLinkedList.toString());
   }
+  
+  @Test
+  public void testToString_whenListEmpty(){
+    assertEquals("[]",singleLinkedList.toString());
+  }
 }
