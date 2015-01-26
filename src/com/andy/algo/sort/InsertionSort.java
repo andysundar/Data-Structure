e3a4d5d46a11ce64de42e97a1b60ae3be7b1cbe5
Anindya Bandopadhyay <anindyabandopadhyay@gmail.com>
2015-01-26 19:04:18 +0530

    Updated Insertion Sort with generic API

--- src/com/andy/algo/sort/InsertionSort.java
+++ src/com/andy/algo/sort/InsertionSort.java
@@ -16,23 +16,42 @@
 
 package com.andy.algo.sort;
 
-public class InsertionSort<T extends Comparable<T>>{
+import java.util.Comparator;
 
-	private T temp;
+public class InsertionSort<T>{
+  
+	public void sort(T[] list,Comparator<? super T> comparator) {
+	  T temp = null;
+    int size = list.length;
+    for(int i = 0; i < size; i++) {
+      int index = i;
+      int subIndex = (i - 1);
+      while(subIndex > -1 && (comparator.compare(list[subIndex], list[index]) == 1)){
+        temp = list[index];
+        list[index] = list[subIndex];
+        list[subIndex] = temp;
+        subIndex--;
+        index--;
+      }
+    }
+  }
+
+	public void sort(Comparable<? super T>[] list) {
+	  Comparable<? super T> temp = null;
+    int size = list.length;
+    for(int i = 0; i < size; i++) {
+      int index = i;
+      int subIndex = (i - 1);     
+      @SuppressWarnings("unchecked")
+      T o = (T) list[index];
+      while(subIndex > -1 && (list[subIndex].compareTo(o)== 1)){
+        temp = list[index];
+        list[index] = list[subIndex];
+        list[subIndex] = temp;
+        subIndex--;
+        index--;
+      }
+    }
+  }
 	
-	public T[] sort(T[] list) {
-		int size = list.length;
-		for(int i = 0; i < size; i++) {
-			int index = i;
-			int subIndex = (i - 1);
-			while(subIndex > -1 && (list[subIndex].compareTo(list[index])== 1)){
-				temp = list[index];
-				list[index] = list[subIndex];
-				list[subIndex] = temp;
-				subIndex--;
-				index--;
-			}
-		}
-		return list;
-	}
 }
