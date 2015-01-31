3f44c8eb224014d4dd8dfb3ab87cc4068b42af0c
Anindya Bandopadhyay <anindyabandopadhyay@gmail.com>
2015-01-31 15:00:00 +0530

    reverse or in ascending order sort added

--- src/com/andy/algo/sort/InsertionSort.java
+++ src/com/andy/algo/sort/InsertionSort.java
@@ -24,12 +24,17 @@ import com.andy.ds.linear.contract.SimpleList;
 public class InsertionSort<T> {
 
   public void sort(T[] objectArray, Comparator<? super T> comparator) {
+    sort(objectArray, comparator, false);
+  }
+
+  public void sort(T[] objectArray, Comparator<? super T> comparator, boolean reverse) {
     T temp = null;
+    int compareWith = (reverse) ? -1 : 1;
     int size = objectArray.length;
     for (int i = 0; i < size; i++) {
       int index = i;
       int subIndex = (i - 1);
-      while (subIndex > -1 && (comparator.compare(objectArray[subIndex], objectArray[index]) == 1)) {
+      while (subIndex > -1 && (comparator.compare(objectArray[subIndex], objectArray[index]) == compareWith)) {
         temp = objectArray[index];
         objectArray[index] = objectArray[subIndex];
         objectArray[subIndex] = temp;
@@ -40,14 +45,19 @@ public class InsertionSort<T> {
   }
 
   public void sort(Comparable<? super T>[] objectArray) {
+    sort(objectArray, false);
+  }
+
+  public void sort(Comparable<? super T>[] objectArray, boolean reverse) {
     Comparable<? super T> temp = null;
+    int compareWith = (reverse) ? -1 : 1;
     int size = objectArray.length;
     for (int i = 0; i < size; i++) {
       int index = i;
       int subIndex = (i - 1);
       @SuppressWarnings("unchecked")
       T o = (T) objectArray[index];
-      while (subIndex > -1 && (objectArray[subIndex].compareTo(o) == 1)) {
+      while (subIndex > -1 && (objectArray[subIndex].compareTo(o) == compareWith)) {
         temp = objectArray[index];
         objectArray[index] = objectArray[subIndex];
         objectArray[subIndex] = temp;
@@ -59,12 +69,17 @@ public class InsertionSort<T> {
 
   @SuppressWarnings("hiding")
   public <T extends Comparable<? super T>> void sort(List<T> list) {
+    sort(list, false);
+  }
+
+  public <T extends Comparable<? super T>> void sort(List<T> list, boolean reverse) {
     T temp = null;
+    int compareWith = (reverse) ? -1 : 1;
     int size = list.size();
     for (int i = 0; i < size; i++) {
       int index = i;
       int subIndex = (i - 1);
-      while (subIndex > -1 && (list.get(subIndex).compareTo(list.get(index)) == 1)) {
+      while (subIndex > -1 && (list.get(subIndex).compareTo(list.get(index)) == compareWith)) {
         temp = list.set(index, list.get(subIndex));
         list.set(subIndex, temp);
         subIndex--;
@@ -72,51 +87,66 @@ public class InsertionSort<T> {
       }
     }
   }
-  
+
   public void sort(List<T> list, Comparator<? super T> comparator) {
+    sort(list, comparator, false);
+  }
+
+  public void sort(List<T> list, Comparator<? super T> comparator, boolean reverse) {
     T temp = null;
+    int compareWith = (reverse) ? -1 : 1;
     int size = list.size();
     for (int i = 0; i < size; i++) {
       int index = i;
       int subIndex = (i - 1);
-      while (subIndex > -1 && (comparator.compare(list.get(subIndex), list.get(index)) == 1)) {
-        temp = list.set(index,list.get(subIndex));
-        list.set(subIndex,temp);
+      while (subIndex > -1 && (comparator.compare(list.get(subIndex), list.get(index)) == compareWith)) {
+        temp = list.set(index, list.get(subIndex));
+        list.set(subIndex, temp);
         subIndex--;
         index--;
       }
     }
   }
-  
-  
+
   @SuppressWarnings("hiding")
   public <T extends Comparable<? super T>> void sort(SimpleList<T> list) {
+    sort(list, false);
+  }
+
+  public <T extends Comparable<? super T>> void sort(SimpleList<T> list, boolean reverse) {
     T temp = null;
+    int compareWith = (reverse) ? -1 : 1;
     int size = list.size();
     for (int i = 0; i < size; i++) {
       int index = i;
       int subIndex = (i - 1);
-      while (subIndex > -1 && (list.get(subIndex).compareTo(list.get(index)) == 1)) {
-        temp =  list.replaceValue(index, list.get(subIndex));
+      while (subIndex > -1 && (list.get(subIndex).compareTo(list.get(index)) == compareWith)) {
+        temp = list.replaceValue(index, list.get(subIndex));
         list.replaceValue(subIndex, temp);
         subIndex--;
         index--;
       }
     }
   }
-  
+
   public void sort(SimpleList<T> list, Comparator<? super T> comparator) {
+    sort(list, comparator, false);
+  }
+
+  public void sort(SimpleList<T> list, Comparator<? super T> comparator, boolean reverse) {
     T temp = null;
+    int compareWith = (reverse) ? -1 : 1;
     int size = list.size();
     for (int i = 0; i < size; i++) {
       int index = i;
       int subIndex = (i - 1);
-      while (subIndex > -1 && (comparator.compare(list.get(subIndex), list.get(index)) == 1)) {
-        temp =  list.replaceValue(index, list.get(subIndex));
+      while (subIndex > -1 && (comparator.compare(list.get(subIndex), list.get(index)) == compareWith)) {
+        temp = list.replaceValue(index, list.get(subIndex));
         list.replaceValue(subIndex, temp);
         subIndex--;
         index--;
       }
     }
   }
+
 }
--- test/com/andy/algo/sort/InsertionSortTest.java
+++ test/com/andy/algo/sort/InsertionSortTest.java
@@ -20,6 +20,7 @@ import static org.junit.Assert.assertFalse;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 import org.junit.After;
 import org.junit.Before;
@@ -31,13 +32,18 @@ import com.andy.ds.linear.contract.SimpleList;
 public class InsertionSortTest {
 
   private InsertionSort<Integer> intSort;
-  private Integer dataForSort[] = { 3, 4, 1, 6, 9, 10, 5, 2, 8, 7 };
+  private Integer dataForSort[] ;
   private List<Integer> list;
   private SimpleList<Integer> simpleList;
 
   @Before
   public void setUp() throws Exception {
     intSort = new InsertionSort<Integer>();
+    Random random = new Random();
+    dataForSort = new Integer[10];
+    for(int index = 0; index < 10; index++){
+      dataForSort[index] = random.nextInt();
+    }
   }
 
   @After
@@ -49,20 +55,40 @@ public class InsertionSortTest {
   @Test
   public void testSort_withArrayParm() {
     intSort.sort(dataForSort);
-    for (int i = 0; i < dataForSort.length; i++) {
-      int nextIndex = (i + 1) == dataForSort.length ? i : (i + 1);
-      assertFalse("Insertion Sort is fail due to " + dataForSort[i] + " before " + dataForSort[nextIndex]
-              + " in result array.", (dataForSort[i] > dataForSort[nextIndex]));
+    for (int index = 0; index < dataForSort.length; index++) {
+      int nextIndex = (index + 1) == dataForSort.length ? index : (index + 1);
+      assertFalse("Insertion Sort is fail due to " + dataForSort[index] + " before " + dataForSort[nextIndex]
+              + " in result array.", (dataForSort[index] > dataForSort[nextIndex]));
+    }
+  }
+  
+  @Test
+  public void testReverseSort_withArrayParm() {
+    intSort.sort(dataForSort,true);
+    for (int index = 0; index < dataForSort.length; index++) {
+      int nextIndex = (index + 1) == dataForSort.length ? index : (index + 1);
+      assertFalse("Insertion Sort is fail due to " + dataForSort[index] + " before " + dataForSort[nextIndex]
+              + " in result array.", (dataForSort[index] < dataForSort[nextIndex]));
     }
   }
 
   @Test
   public void testSort_withArrayAndComparator() {
     intSort.sort(dataForSort, new WrapperIntegerComparator());
-    for (int i = 0; i < dataForSort.length; i++) {
-      int nextIndex = (i + 1) == dataForSort.length ? i : (i + 1);
-      assertFalse("Insertion Sort is fail due to " + dataForSort[i] + " before " + dataForSort[nextIndex]
-              + " in result array.", (dataForSort[i] > dataForSort[nextIndex]));
+    for (int index = 0; index < dataForSort.length; index++) {
+      int nextIndex = (index + 1) == dataForSort.length ? index : (index + 1);
+      assertFalse("Insertion Sort is fail due to " + dataForSort[index] + " before " + dataForSort[nextIndex]
+              + " in result array.", (dataForSort[index] > dataForSort[nextIndex]));
+    }
+  }
+  
+  @Test
+  public void testReverseSort_withArrayAndComparator() {
+    intSort.sort(dataForSort, new WrapperIntegerComparator(),true);
+    for (int index = 0; index < dataForSort.length; index++) {
+      int nextIndex = (index + 1) == dataForSort.length ? index : (index + 1);
+      assertFalse("Insertion Sort is fail due to " + dataForSort[index] + " before " + dataForSort[nextIndex]
+              + " in result array.", (dataForSort[index] < dataForSort[nextIndex]));
     }
   }
   
@@ -77,8 +103,23 @@ public class InsertionSortTest {
       assertFalse("Insertion Sort is fail due to " + list.get(index) + " before " + list.get(nextIndex)
               + " in result array.", (list.get(index) > list.get(nextIndex)));
     }
+    //System.out.printf("%s result \n %s\n","Insertion Sort of List ",list.toString());
   }
 
+  @Test
+  public void testReverseSort_withListParm() {
+    list = new ArrayList<Integer>(dataForSort.length);
+    populateListFromArray();
+    intSort.sort(list,true);
+    int size = list.size();
+    for (int index = 0; index < size; index++) {
+      int nextIndex = (index + 1) == size ? index : (index + 1);
+      assertFalse("Insertion Sort is fail due to " + list.get(index) + " before " + list.get(nextIndex)
+              + " in result array.", (list.get(index) < list.get(nextIndex)));
+    }
+    //System.out.printf("%s result \n %s\n","Insertion Reverse Sort of List ",list.toString());
+  }
+  
   @Test
   public void testSort_withListAndComparator() {
     list = new ArrayList<Integer>(dataForSort.length);
@@ -89,7 +130,22 @@ public class InsertionSortTest {
       int nextIndex = (index + 1) == size ? index : (index + 1);
       assertFalse("Insertion Sort is fail due to " + list.get(index) + " before " + list.get(nextIndex)
               + " in result array.", (list.get(index) > list.get(nextIndex)));
-    }
+    }    
+    //System.out.printf("%s result \n %s\n","Insertion Sort of List using Comparator",list.toString());
+  }
+  
+  @Test
+  public void testReverseSort_withListAndComparator() {
+    list = new ArrayList<Integer>(dataForSort.length);
+    populateListFromArray();
+    intSort.sort(list,new WrapperIntegerComparator(),true);
+    int size = list.size();
+    for (int index = 0; index < size; index++) {
+      int nextIndex = (index + 1) == size ? index : (index + 1);
+      assertFalse("Insertion Sort is fail due to " + list.get(index) + " before " + list.get(nextIndex)
+              + " in result array.", (list.get(index) < list.get(nextIndex)));
+    }    
+    //System.out.printf("%s result \n %s\n","Insertion Reverse Sort of List using Comparator",list.toString());
   }
   
   @Test
@@ -102,6 +158,20 @@ public class InsertionSortTest {
       assertFalse("Insertion Sort is fail due to " + simpleList.get(index) + " before " + simpleList.get(nextIndex)
               + " in result array.", (simpleList.get(index) > simpleList.get(nextIndex)));
     }
+    //System.out.printf("%s result \n %s\n","Insertion Sort of SimpleList ",simpleList.toString());
+  }
+  
+  @Test
+  public void testReverseSort_withSimpleListParm() {
+    simpleList = new SingleLinkedList<Integer>(dataForSort);
+    intSort.sort(simpleList,true);
+    int size = simpleList.size();
+    for (int index = 0; index < size; index++) {
+      int nextIndex = (index + 1) == size ? index : (index + 1);
+      assertFalse("Insertion Sort is fail due to " + simpleList.get(index) + " before " + simpleList.get(nextIndex)
+              + " in result array.", (simpleList.get(index) < simpleList.get(nextIndex)));
+    }
+    //System.out.printf("%s result \n %s\n","Insertion Reverse Sort of SimpleList ",simpleList.toString());
   }
 
   @Test
@@ -114,6 +184,20 @@ public class InsertionSortTest {
       assertFalse("Insertion Sort is fail due to " + simpleList.get(index) + " before " + simpleList.get(nextIndex)
               + " in result array.", (simpleList.get(index) > simpleList.get(nextIndex)));
     }
+    //System.out.printf("%s result \n %s\n","Insertion Sort of SimpleList using Comparator",simpleList.toString());
+  }
+  
+  @Test
+  public void testReverseSort_withSimpleListAndComparator() {
+    simpleList = new SingleLinkedList<Integer>(dataForSort);
+    intSort.sort(simpleList, new WrapperIntegerComparator(),true);
+    int size = simpleList.size();
+    for (int index = 0; index < size; index++) {
+      int nextIndex = (index + 1) == size ? index : (index + 1);
+      assertFalse("Insertion Sort is fail due to " + simpleList.get(index) + " before " + simpleList.get(nextIndex)
+              + " in result array.", (simpleList.get(index) < simpleList.get(nextIndex)));
+    }
+    //System.out.printf("%s result \n %s\n","Insertion Reverse Sort of SimpleList using Comparator",simpleList.toString());
   }
 
   private void populateListFromArray() {
@@ -123,4 +207,5 @@ public class InsertionSortTest {
       }
     }
   }
+  
 }
